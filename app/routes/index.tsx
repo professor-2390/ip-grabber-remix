2195185c4e012d29392d8e392b147b7c78b144bf
Professor-2390 <sadmansakibkhan4@gmail.com>
2022-04-26 04:14:43 +0600

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,47 @@
+import { Button, Container, TextInput } from "@mantine/core";
+import { Form } from "@remix-run/react";
+import type { ActionFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+import { customAlphabet } from "nanoid";
+
+const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz1234567890", 10);
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const redirectUrl = form.get("redirectUrl");
+
+  if (typeof redirectUrl !== "string") {
+    throw new Error("Invalid redirectUrl");
+  }
+
+  const fields = { redirectUrl, trackId: nanoid() };
+
+  const track = await db.track.create({
+    data: fields,
+  });
+
+  return redirect(`/tracks/${track.trackId}`);
+};
+
+export default function Index() {
+  return (
+    <Container>
+      <Form method="post" style={{ display: "flex", alignItems: "flex-end" }}>
+        <TextInput
+          size="xl"
+          placeholder="https://youtube.com"
+          label="Redirect URL"
+          required
+          name="redirectUrl"
+          style={{ flex: 1 }}
+          type="url"
+        />
+
+        <Button color="dark" radius="md" ml="md" size="xl" type="submit">
+          Create tracker
+        </Button>
+      </Form>
+    </Container>
+  );
+}
---
+++ app/routes/tracks/$trackId.tsx
@@ -0,0 +1,83 @@
+import type { Click } from "@prisma/client";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+import { Button, Table, Container, Anchor } from "@mantine/core";
+import { useClipboard } from "@mantine/hooks";
+
+type LoaderData = {
+  track: {
+    trackId: string;
+    clicks: Click[];
+  };
+};
+export const loader: LoaderFunction = async ({ params }) => {
+  const track = await db.track.findUnique({
+    where: { trackId: params.trackId },
+    select: {
+      trackId: true,
+      clicks: {
+        orderBy: [
+          {
+            createdAt: "desc",
+          },
+        ],
+      },
+    },
+  });
+
+  if (!track) {
+    throw new Error(`Track ${params.trackId} does not exist`);
+  }
+
+  const data: LoaderData = { track };
+
+  return json(data);
+};
+
+export default function TrackRoute() {
+  const data = useLoaderData<LoaderData>();
+  const clipboard = useClipboard({ timeout: 500 });
+
+  const rows = data.track.clicks.map((click) => {
+    return (
+      <tr key={click.id}>
+        <td>
+          {new Date(click.createdAt).toLocaleDateString([], { hour12: true })}
+        </td>
+        <td>{click.userAgent}</td>
+        <td>{click.ip}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+    <Anchor href="/">
+      Return Home
+    </Anchor>
+      <Container>
+        <Table verticalSpacing="sm" fontSize="md">
+          <thead>
+            <tr>
+              <td>Date</td>
+              <td>User agent</td>
+              <td>IP</td>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
+
+        <Button
+          color={clipboard.copied ? "green" : "dark"}
+          onClick={() =>
+            clipboard.copy(`${window.location.origin}/${data.track.trackId}`)
+          }
+        >
+          {clipboard.copied ? "Copied!" : "Copy Url"}
+        </Button>
+      </Container>
+    </div>
+  );
+}
